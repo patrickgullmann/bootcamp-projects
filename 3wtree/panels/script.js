79b2d594102ee36f4429625da53bf8cf03fe73f2
Patrick Gullmann <gullmannpatrick@gmail.com>
2022-02-21 18:19:10 +0100

    Hausaufgaben

---
+++ 3wtree/panels/script.js
@@ -0,0 +1,48 @@
+var slide = $("#slide");
+var panel1 = $("#panel1");
+var box = $("#box");
+
+//mousemove in the box
+slide.on("mousedown", function (e) {
+    e.preventDefault();
+    box.on("mousemove", function (e) {
+        if (e.offsetX > slide.outerWidth()) {
+            slide.css({
+                left: e.offsetX + "px",
+            });
+            panel1.css({
+                width: e.offsetX + "px",
+            });
+        }
+    });
+});
+
+slide.on("mouseup", function (e) {
+    e.preventDefault();
+    box.off("mousemove");
+});
+
+// var slide = document.getElementById("slide");
+// var panel1 = document.getElementById("panel1");
+// var box = document.getElementById("box");
+
+// //mousemove in the box
+// slide.addEventListener("mousedown", function (e) {
+//     e.preventDefault();
+//     box.addEventListener("mousemove", function (e) {
+//         if (e.offsetX > slide.offsetWidth) {
+//             slide.style.left = e.offsetX + "px";
+//             panel1.style.width = e.offsetX + "px";
+//         }
+//     });
+// });
+
+// slide.addEventListener("mouseup", function (e) {
+//     e.preventDefault();
+//     box.removeEventListener("mousemove", function (e) {
+//         if (e.offsetX > slide.offsetWidth) {
+//             slide.style.left = e.offsetX + "px";
+//             panel1.style.width = e.offsetX + "px";
+//         }
+//     });
+// });
