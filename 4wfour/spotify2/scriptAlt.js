3427ef97168f804f8597e090d94aecb40ace7491
Patrick Gullmann <gullmannpatrick@gmail.com>
2022-03-01 12:16:42 +0100

    Spotify Search Infitie Scroll

--- 4wfour/spotify2/scriptAlt.js
+++ 4wfour/spotify2/scriptAlt.js
@@ -1,5 +1,6 @@
 (function () {
     var moreButton = $(".more-button");
+    //WICHTIG: wir müssen einfach nach jedem click die next adresse hier speichern und im button more neu abrufen
     var next = "";
 
     $(".submit-button").on("click", function () {
@@ -10,11 +11,13 @@
             method: "GET",
             url: "https://spicedify.herokuapp.com/spotify",
             data: {
-                query: userInput,
-                type: artistOrAlbum,
+                query: userInput, //spotify demands this
+                type: artistOrAlbum, //spotify demands that is called like this e.g. artist or album
             },
             success: function (response) {
-                response = response.artists || response.albums;
+                //note spotify gives me by default first 20 search result, and a next propertiy
+
+                response = response.artists || response.albums; //could write this as as if response.artists == true make response = response.artist else make response.album bc we could get back both
 
                 var resultsHtml = "";
                 for (var i = 0; i < response.items.length; i++) {
@@ -26,6 +29,7 @@
                     }
 
                     resultsHtml +=
+                        //here wrapping of img in a Tag
                         "<a href='" +
                         response.items[i].external_urls.spotify +
                         "'>" +
@@ -33,6 +37,7 @@
                         defaultImage +
                         "' alt='image' >" +
                         "</a>" +
+                        //here now Text in a Tag
                         "<a href='" +
                         response.items[i].external_urls.spotify +
                         "'>" +
@@ -41,93 +46,30 @@
                 }
 
                 $(".results-container").html(resultsHtml);
+                moreButton.show();
 
-                /* <----------------------NEUER TEIL für infinite scroll ------------------------>*/
-                // location.search is ?scroll=infinite und wenn größer -1> bedeutet es hat nen Index im String
-                if (location.search.indexOf("?scroll=infinite") > -1) {
-                    // wenn drin stehen haben, call new function to check the user is near the bottom of the page
-                    checkScrollPos();
-                } else {
-                    // anonsten -> wenn nicht in URL stehen haben dann normal weitere mit Button Laden
-                    moreButton.show();
-                }
-                /* <----------------------Ende Neuer Teil ------------------------------------>*/
-
+                //need to check both true (bc if there is no next!!!) that doe does not break
                 var nextUrl =
                     response.next &&
                     response.next.replace(
                         "api.spotify.com/v1/search",
                         "spicedify.herokuapp.com/spotify"
-                    );
-                next = nextUrl;
+                    ); //da wir nicht direkt auf Spotify können;
+                next = nextUrl; //returns for next get request
             },
         });
     });
 
-    function checkScrollPos() {
-        setTimeout(function () {
-            if (
-                // zb ganz unten: wie weit von oben (im Dokument) + höhe window == höhe document
-                $(document).scrollTop() + $(window).height() >
-                $(document).height() - 300
-            ) {
-                // wenn User 300px oder näher am Boden -> Ajax request um neue zu laden! -> nicht mehr butten, aber selber code wie in button
-                $.ajax({
-                    method: "GET",
-                    url: next,
-                    success: function (response) {
-                        response = response.artists || response.albums;
-
-                        var resultsHtml = "";
-                        for (var i = 0; i < response.items.length; i++) {
-                            var defaultImage =
-                                "https://www.hdwallpaper.nu/wp-content/uploads/2015/02/o-BROKEN-HEART-facebook676-660x330.jpg";
-
-                            if (response.items[i].images.length > 0) {
-                                defaultImage = response.items[i].images[0].url;
-                            }
-
-                            resultsHtml +=
-                                "<a href='" +
-                                response.items[i].external_urls.spotify +
-                                "'>" +
-                                "<img src='" +
-                                defaultImage +
-                                "' alt='image' >" +
-                                "</a>" +
-                                "<a href='" +
-                                response.items[i].external_urls.spotify +
-                                "'>" +
-                                response.items[i].name +
-                                "</a>";
-                        }
-
-                        $(".results-container").append(resultsHtml);
-
-                        var nextUrl =
-                            response.next &&
-                            response.next.replace(
-                                "api.spotify.com/v1/search",
-                                "spicedify.herokuapp.com/spotify"
-                            );
-                        next = nextUrl;
-                        // also call it again after we did the request!!! -> muss ja immer laufen
-                        checkScrollPos();
-                    },
-                });
-            } else {
-                // call this funciton again to check the scroll position
-                checkScrollPos();
-            }
-        }, 500);
-    }
-
     moreButton.on("click", function () {
+        //wenn hier geklickt wurde, dann haben wir ein next durch ajax erhalten
         $.ajax({
             method: "GET",
             url: next,
+            //wir müssen neues data nicht definieren!!! ist bei next schon enthalten
             success: function (response) {
-                response = response.artists || response.albums;
+                //note spotify gives me by default first 20 search result, and a next propertiy
+
+                response = response.artists || response.albums; //could write this as as if response.artists == true make response = response.artist else make response.album bc we could get back both
 
                 var resultsHtml = "";
                 for (var i = 0; i < response.items.length; i++) {
@@ -139,6 +81,7 @@
                     }
 
                     resultsHtml +=
+                        //here wrapping of img in a Tag
                         "<a href='" +
                         response.items[i].external_urls.spotify +
                         "'>" +
@@ -146,6 +89,7 @@
                         defaultImage +
                         "' alt='image' >" +
                         "</a>" +
+                        //here now Text in a Tag
                         "<a href='" +
                         response.items[i].external_urls.spotify +
                         "'>" +
@@ -163,21 +107,33 @@
                         "spicedify.herokuapp.com/spotify"
                     );
                 next = nextUrl;
+                //setUrl(response.next); //könnten auch als Funktion machen, und oben natürlich auch
             },
         });
     });
+
+    // function setUrl(next) {
+    //     var nextUrl =
+    //         next &&
+    //         next.replace(
+    //             "api.spotify.com/v1/search",
+    //             "spicedify.herokuapp.com/spotify"
+    //         ); //da wir nicht direkt auf Spotify können;
+    //     next = nextUrl; //change for next get request
+    // }
 })();
+
 
 //add to the url ?scroll=infinite
 //location.search gives the string
 
-//incode check for querey string if so, hide more button and activite infite
+//incode check for querey string if so, hide more button and activite infite 
 
 //location.search.indexOf("scroll=infite") if eequals 1 than it fits or > -1 just means it is somewhere
 
 // window.addEvenetListener("scroll", function(){}) firest when scrolling BUT DONT DO IT -> Fires to much
 
-//if user reached the bottum with the mouse
+//if user reached the bottum with the mouse 
 
 // $(document).scrollTop() //how far we are aways from the top
 
